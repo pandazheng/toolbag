bd7a480c55b64737a9246c2f2effda91e3ac1222
t4intz <t4intz@gmail.com>
2013-12-25 14:11:50 +0800

    fix xrange overflow error

--- toolbag/analysis.py
+++ toolbag/analysis.py
@@ -8,6 +8,51 @@ import idautils
 import idaapi
 import idc
 
+class myxrange(object):
+    def __init__(self, a1, a2=None, step=1):
+        if step == 0:
+            raise ValueError("arg 3 must not be 0")
+        if a2 is None:
+            a1, a2 = 0, a1
+        if (a2 - a1) % step != 0:
+            a2 += step - (a2 - a1) % step
+        if cmp(a1, a2) != cmp(0, step):
+            a2 = a1
+        self.start, self.stop, self.step = a1, a2, step
+
+    def __iter__(self):
+        n = self.start
+        while cmp(n, self.stop) == cmp(0, self.step):
+            yield n
+            n += self.step
+
+    def __repr__(self):
+        return "MyXRange(%d,%d,%d)" % (self.start, self.stop, self.step)
+
+    # NB: len(self) will convert this to an int, and may fail
+    def __len__(self):
+        return (self.stop - self.start)//(self.step)
+
+    def __getitem__(self, key):
+        if key < 0:
+            key = self.__len__() + key
+            if key < 0:
+                raise IndexError("list index out of range")
+            return self[key]
+        n = self.start + self.step*key
+        if cmp(n, self.stop) != cmp(0, self.step):
+            raise IndexError("list index out of range")
+        return n
+
+    def __reversed__(self):
+        return MyXRange(self.stop-self.step, self.start-self.step, -self.step)
+
+    def __contains__(self, val):
+        if val == self.start: return cmp(0, self.step) == cmp(self.start, self.stop)
+        if cmp(self.start, val) != cmp(0, self.step): return False
+        if cmp(val, self.stop) != cmp(0, self.step): return False
+        return (val - self.start) % self.step == 0
+
 class properties():
 
 	def __init__(self, addr):
@@ -66,7 +111,7 @@ class properties():
 			if lastM - firstM > 0x1000:
 				return
 
-			for i in xrange(firstM, lastM):
+			for i in myxrange(firstM, lastM):
 				mName = idc.GetMemberName(sid, i)
 				mSize = idc.GetMemberSize(sid, i)
 				mFlag = idc.GetMemberFlag(sid, i)
@@ -77,7 +122,7 @@ class properties():
 					reg_off = off
 
 			# XXX: just store the data, dont loop twice.
-			for i in xrange(firstM, lastM):
+			for i in myxrange(firstM, lastM):
 				mName = idc.GetMemberName(sid, i)
 				mSize = idc.GetMemberSize(sid, i)
 				mFlag = idc.GetMemberFlag(sid, i)
